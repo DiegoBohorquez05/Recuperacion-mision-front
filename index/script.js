a6fd916289a730c6f3a5567c4f8bffa1c5ad970e
DiegoBohorquez05 <113608171+DiegoBohorquez05@users.noreply.github.com>
2023-01-25 09:46:09 -0500

    entregar

--- index/script.js
+++ index/script.js
@@ -1,4 +1,4 @@
-count=0
+count=1
 const botonCrear = document.getElementById("crearCliente");
 const Cliente = document.getElementById("tabla");
 
@@ -39,7 +39,7 @@ botonCrear.addEventListener("click", () => {
     
     id=count++
 
-    tabla.innerHTML += `<tr><th scope="row">${Nombre}</th><td>${Apellido}</td><td>${Nit}</td><td>${Direccion}</td><td>${Ciudad}</td><td>${Telefono}</td><td>${CD}</td><td>Activado</td><td>
+    tabla.innerHTML += `<tr><th scope="row" id="id">${id}</th><th scope="row">${Nombre}</th><td>${Apellido}</td><td>${Nit}</td><td>${Direccion}</td><td>${Ciudad}</td><td>${Telefono}</td><td>${CD}</td><td>Activado</td><td>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16" id="editar">
         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
         </svg>
@@ -53,33 +53,25 @@ botonCrear.addEventListener("click", () => {
         e.target.parentNode.parentNode.parentNode.remove()
     })
 
-    // const editar = document.getElementById("editar");
+     const editar = document.getElementById("editar");
 
-
-    // editar.addEventListener("click", () => {
-    //   console.log("editar");
-    // });
-
-    // eliminar.addEventListener("click", () => {
-    //     console.log('aqui se elimino')
-    //   let NombreEli = document.getElementById(`.fila-${eliminar}`);
-    //   NombreEli.remove();
-    // });
-
-    // const buscar = document.getElementById("buscar");
-    // const valorBuscar = document.getElementById("valor");
-
-    // valorBuscar.addEventListener("click", () => {
-    //   console.log("a");
-    // });
-
-    // buscar.addEventListener("click", () => {
-    //   console.log("x");
-    //   // if(valorBuscar!=''){
-    //   //     console.log('buscar')
-    //   // }
-    // });
+     editar.addEventListener("click", () => {
+       console.log("editar");
+     });
   } else {
     alert("Formulario incompleto");
   }
 });
+
+  const buscar = document.getElementById("buscar");
+  const valorBuscar = document.getElementById("valor").value;
+  buscar.addEventListener("click", () => {
+     if(valorBuscar!=''){
+        console.log('se esta buscando algo')
+      if(valorBuscar===tabla){
+        console.log(valorBuscar)
+      }
+     }else{
+      alert('No hay nada por buscar')
+     }
+  });
